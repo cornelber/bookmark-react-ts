13b717d56cb184f82b37018eba40836fe653c850
cornelber <bercornel@gmail.com>
2024-09-20 14:41:39 +0300

    added 404 page and refactored home page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mt-40 w-screen">
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="mb-8">Sorry, the page you are looking for does not exist.</p>
+      <Link to="/" className="text-primary-blue hover:underline">
+        Go back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
